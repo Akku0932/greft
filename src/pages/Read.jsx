7fb81c6c4c96f180a271d5d67f7f9695b80a784c
Akku0932 <shouryatyagi999@gmail.com>
2025-10-11 09:57:42 -0700

    Describe your change

--- src/pages/Read.jsx
+++ src/pages/Read.jsx
@@ -1,5 +1,5 @@
 import { useParams, useSearchParams } from 'react-router-dom'
-import { api } from '../lib/api.js'
+import { api, getImage, parseIdTitle, sanitizeTitleId, pickImage } from '../lib/api.js'
 import { upsertProgress } from '../lib/progressApi'
 import { upsertRecentRead } from '../lib/recentReadsApi'
 import { getReadUrl, getInfoUrl } from '../lib/urlUtils'
@@ -12,7 +12,7 @@ export default function Read() {
     <div className="min-h-screen bg-gray-100 dark:bg-gray-900">
       <div className="max-w-4xl mx-auto p-4">
         <h1 className="text-2xl font-bold text-gray-900 dark:text-white mb-4">
-          Read Page Test - URLUtils Import Added
+          Read Page Test - All API Functions Import Added
         </h1>
         <p className="text-gray-600 dark:text-gray-300">
           ID: {id}
@@ -37,6 +37,9 @@ export default function Read() {
         </p>
         <p className="text-gray-600 dark:text-gray-300">
           URLUtils imported: {getReadUrl && getInfoUrl ? 'Yes' : 'No'}
+        </p>
+        <p className="text-gray-600 dark:text-gray-300">
+          API Functions imported: {getImage && parseIdTitle && sanitizeTitleId && pickImage ? 'Yes' : 'No'}
         </p>
       </div>
     </div>
